ebc0b017d90a925c48006c7aca7af7da264b2ca5
alinamvarkallan1390-source <alinamvarkallan1390@gmail.com>
2026-07-31 18:31:41 +0330

    Update build.gradle.kts

--- SmartConnect/wear/build.gradle.kts
+++ SmartConnect/wear/build.gradle.kts
@@ -37,22 +37,24 @@ composeOptions {
     kotlinCompilerExtensionVersion = "1.5.14"
 }
 }
-
 dependencies {
     implementation(project(":shared"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
+
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.timber)
     implementation(libs.gson)
     implementation(libs.coroutines.android)
-    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.work.runtime.ktx)
 }
